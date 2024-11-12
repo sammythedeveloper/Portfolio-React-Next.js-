257c853cd92c6d4b1f8891a213b200ce109c4e52
sammythedeveloper <justsammy484@gmail.com>
2024-11-12 07:07:05 -0500

    tape-section

--- src/sections/Testimonials.tsx
+++ src/sections/Testimonials.tsx
@@ -3,40 +3,58 @@ import memojiAvatar2 from "@/assets/images/memoji-avatar-2.png";
 import memojiAvatar3 from "@/assets/images/memoji-avatar-3.png";
 import memojiAvatar4 from "@/assets/images/memoji-avatar-4.png";
 import memojiAvatar5 from "@/assets/images/memoji-avatar-5.png";
+import Image from "next/image";
 
 const testimonials = [
   {
-    name: "Alex Turner",
-    position: "Marketing Manager @ TechStartups",
-    text: "Alex was instrumental in transforming our website into a powerful marketing tool. His attention to detail and ability to understand our brand is exceptional. We're thrilled with the results!",
+    name: "Adugna Bekele",
+    position: "CEO @ EvangadiTech",
+    text: "Samson was instrumental at Evangadi. His attention to detail and ability to understand to solve problem is exceptional. We're thrilled with his results!",
     avatar: memojiAvatar1,
   },
   {
-    name: "Olivia Green",
-    position: "Head of Design @ GreenLeaf",
-    text: "Working with Alex was a pleasure. His expertise in frontend development brought our designs to life in a way we never imagined. The website has exceeded our expectations.",
-    avatar: memojiAvatar2,
+    name: "Danny Y.",
+    position: "Instractor @ EvangadiTech",
+    text: "Working with Samson was a pleasure. His expertise in frontend development brought amazing designs to life in a way we never imagined. The website has exceeded our expectations.",
+    avatar: memojiAvatar5,
   },
   {
-    name: "Daniel White",
-    position: "CEO @ InnovateCo",
-    text: "Alex's ability to create seamless user experiences is unmatched. Our website has seen a significant increase in conversions since launching the new design. We couldn't be happier.",
+    name: "Biruk K.",
+    position: "Full Stack Engineer @ EvangadiTech ",
+    text: "Samson's ability to create seamless user experiences is unmatched. Our freelance projects has seen a significant increase in conversions since launching the new design. We couldn't be happier.",
     avatar: memojiAvatar3,
   },
   {
-    name: "Emily Carter",
-    position: "Product Manager @ GlobalTech",
-    text: "Alex is a true frontend wizard. He took our complex product and transformed it into an intuitive and engaging user interface. We're already seeing positive feedback from our customers.",
-    avatar: memojiAvatar4,
-  },
-  {
-    name: "Michael Brown",
-    position: "Director of IT @ MegaCorp",
-    text: "Alex's work on our website has been nothing short of exceptional. He's a talented developer who is also a great communicator. We highly recommend him.",
+    name: "Selam G.",
+    position: "CEO@ TibebDesign",
+    text: "Samson's work on our website has been nothing short of exceptional. He's a talented developer who is also a great communicator. We highly recommend him.",
     avatar: memojiAvatar5,
   },
+  {
+    name: "Michael Carter",
+    position: "Former Client",
+    text: "Samson is a true frontend wizard. He took our complex idea and transformed it into an intuitive and engaging user interface. I am already seeing positive feedback after using his application.",
+    avatar: memojiAvatar4,
+  }
 ];
 
 export const TestimonialsSection = () => {
-  return <div>Testimonials Section</div>;
+  return <div>
+    <p>Happy Clients</p>
+    <p>What Clients Say about me</p>
+    <p>
+      Don't just take my word for it.See what people know me have to say about my work
+    </p>
+    <div>
+      {testimonials.map((testimonial) => (
+        <div key={testimonial.name}>
+          <Image src={testimonial.avatar} alt={testimonial.name} />
+          <div>{testimonial.name}</div>
+          <div>{testimonial.position}</div>
+          <p>{testimonial.text}</p>
+        </div>
+        
+      ))}
+</div>
+  </div>;
 };
