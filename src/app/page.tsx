fa149fdc72874d31c2b31d36f04eba4b779ce835
sammythedeveloper <justsammy484@gmail.com>
2024-11-13 22:56:37 -0500

    footer

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -4,6 +4,8 @@ import { ProjectsSection } from "@/sections/Projects";
 import { TapeSection } from "@/sections/Tape";
 import { TestimonialsSection } from "@/sections/Testimonials";
 import { AboutSection } from "@/sections/About";
+import { ContactSection } from "@/sections/Contact"
+import { Footer } from "@/sections/Footer";
 
 export default function Home() {
   return (
@@ -13,7 +15,9 @@ export default function Home() {
       <ProjectsSection />
       <TapeSection />
       <TestimonialsSection />
-      <AboutSection/>
+      <AboutSection />
+      <ContactSection />
+      <Footer/>
     </div>
   );
 }
